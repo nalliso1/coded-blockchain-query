33ac5fbaee9d4ab9e09c259e37e3f1c2a4a6e66d
dmudugam <dineth.m@unb.ca>
2025-03-06 10:20:46 -0400

    CS4545: Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='coded-blockchain-query',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Dineth Mudugamuwa Hewage',
+    author_email='dineth.m@unb.ca',
     description='A project for implementing coded blockchain based range queries on historical data.',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
-        # List your project dependencies here
+        'Flask==2.0.1',
+        'numpy==1.21.0',
+        'pandas==1.3.0',
+        'requests==2.25.1',
+        'pytest==6.2.4',
+        'pycryptodome==3.10.1',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
